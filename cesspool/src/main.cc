7e8ea0914beab0935549cb2e7848732b86c7f46e
Mateusz Miekicki <miekickimateusz@gmail.com>
2025-07-05 23:48:42 +0200

    working distance measurement

--- cesspool/src/main.cc
+++ cesspool/src/main.cc
@@ -1,45 +1,44 @@
-#include <freertos/FreeRTOS.h>
-#include <driver/gpio.h>
-#include <initializer_list>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_timer.h"
 #include <iostream>
-#include <map>
 
-void resetAndSetOutputMode(gpio_num_t pin)
-{
-    gpio_reset_pin(pin);
-    gpio_set_direction(pin, GPIO_MODE_OUTPUT);
-}
+#define TRIG_PIN GPIO_NUM_2
+#define ECHO_PIN GPIO_NUM_1
 
 extern "C" void app_main(void)
 {
-    std::map<std::string, gpio_num_t> leds = {
-        {"red",
-         GPIO_NUM_3},
-        {"green",
-         GPIO_NUM_4},
-        {"blue",
-         GPIO_NUM_5},
-        {"warmWhite",
-         GPIO_NUM_18},
-        {"coldWhite",
-         GPIO_NUM_19}};
+    std::cout << "start0";
+    gpio_set_direction(TRIG_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(ECHO_PIN, GPIO_MODE_INPUT);
+    std::cout << "start1";
 
-    for (const auto&led : leds)
+    while (1)
     {
-        resetAndSetOutputMode(led.second);
-    }
-    for (;;)
-    {
-        for (const auto& led : leds)
-        {
-            std::cout << led.first << std::endl;
-            std::cout << "enable" << std::endl;
-            gpio_set_level(led.second, 1);
-            vTaskDelay(100);
-            std::cout << "disable" << std::endl;
-            gpio_set_level(led.second, 0);
-            vTaskDelay(100);
-            std::cout << "----------\n";
-        }
+        std::cout<<"start";
+        // Send trigger pulse
+        gpio_set_level(TRIG_PIN, 0);
+        vTaskDelay(2);
+        gpio_set_level(TRIG_PIN, 1);
+        vTaskDelay(10);
+        gpio_set_level(TRIG_PIN, 0);
+
+        // Measure echo pulse width
+        // uint64_t start_time = esp_timer_get_time();
+        std::cout<<"pierwszy while";
+        while (!gpio_get_level(ECHO_PIN))
+            ; // Wait for HIGH
+        uint64_t echo_start = esp_timer_get_time();
+        std::cout<<"drugi while";
+        while (gpio_get_level(ECHO_PIN))
+            ;
+        uint64_t echo_end = esp_timer_get_time();
+
+        uint64_t duration = echo_end - echo_start;
+        double distance = ((duration * 348.0) / 2)/10000;
+
+        std::cout<<"Distance:"<<distance<<std::endl;
+        vTaskDelay(50);
     }
 }
