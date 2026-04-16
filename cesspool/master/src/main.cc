42f0eac28c60ec7a899d494315bdad718f56b5ea
Mateusz Miekicki <miekickimateusz@gmail.com>
2026-04-16 22:08:49 +0000

    added missing function

--- cesspool/master/src/main.cc
+++ cesspool/master/src/main.cc
@@ -5,6 +5,13 @@
 #include "qpcpp.hpp"
 #include "platform/bsp/storage/nvs/NonVolatileStorage.hh"
 
+namespace QP {
+// Required by ports/esp-idf/qf_port.cpp (called from QF::run / framework startup).
+void QF::onStartup() {}
+
+void QF::onCleanup() {}
+}  // namespace QP
+
 extern "C" void app_main() {
     platform::bsp::NonVolatileStorage storage;
     auto result = storage.initialize();
@@ -12,5 +19,6 @@ extern "C" void app_main() {
         ESP_LOGE("app_main", "Failed to initialize storage: %d", static_cast<int>(result.error()));
         return;
     }
+    QP::QF::init();
     QP::QF::run();
 }
