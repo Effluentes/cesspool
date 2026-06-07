79e077230f4ca4831a7ecf239ad2e0985f189019
Mateusz Miekicki <miekickimateusz@gmail.com>
2026-06-07 10:25:35 +0000

    remove unused part

--- cesspool/master/src/main.cc
+++ cesspool/master/src/main.cc
@@ -1,18 +1,13 @@
 #include "esp_log.h"
 #include "esp_timer.h"
-// #include "actors/Blinker.hh"
 #include "actors/bluetoothManagerAO/BluetoothManagerAO.hh"
 #include "esp_nimble_hci.h"
 
 
-// ---------------------------------------------------------------------------
 namespace QP {
 static const char* TAG = "QP";
-void QP::QF::onStartup()
-{
-    ESP_LOGI("QP", "QF started");
-
-}    void QF::onCleanup() {}
+void QP::QF::onStartup() { ESP_LOGI("QP", "QF started"); }
+void QF::onCleanup() {}
 }
 
 // ---------------------------------------------------------------------------
@@ -32,19 +27,6 @@ extern "C" void app_main() {
     static uint8_t pool[500 * sizeof(QP::QEvt)];
     QP::QF::poolInit(pool, sizeof(pool), sizeof(QP::QEvt));
 
-
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-
-    // Tick
-    static auto tick_cb = [](void*) { QP::QF::tickX_(0U); };
-    esp_timer_handle_t timer;
-    esp_timer_create_args_t args = {
-        .callback = tick_cb,
-        .dispatch_method = ESP_TIMER_TASK,
-        .name = "tick"
-    };
-    esp_timer_create(&args, &timer);
-    esp_timer_start_periodic(timer, 10'000);
 
     static QP::QSubscrList subscrSto[BT_MAX_SIG];
     QP::QF::psInit(subscrSto, Q_DIM(subscrSto));
